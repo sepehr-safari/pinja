8dfe523ad6edcb6d50b592e7b00bdb203b2ec4b8
sepehr-safari <safari.sepehr@gmail.com>
2025-02-01 12:45:31 +0330

    simplify HomePage component by removing active user check and always rendering PinsFeedWidget

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,9 @@
-import { useActiveUser } from 'nostr-hooks';
-
 import { PinsFeedWidget } from '@/features/pins-feed-widget';
 
 export const HomePage = () => {
-  const { activeUser } = useActiveUser();
-
   return (
     <>
-      {activeUser ? (
-        <PinsFeedWidget />
-      ) : (
-        <div className="flex flex-col h-full w-full items-center justify-center">
-          <img src="/pinja.png" alt="Pinja logo" className="rounded-full" />
-
-          <h2>Welcome to Pinja</h2>
-          <p>Log-in to get started</p>
-        </div>
-      )}
+      <PinsFeedWidget />
     </>
   );
 };
