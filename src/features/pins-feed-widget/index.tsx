edbec9383882e718145dcb71ae85154b99b5df1a
sepehr-safari <safari.sepehr@gmail.com>
2025-02-05 09:31:18 +0330

    Hide new pin widget when user is not signed in

--- src/features/pins-feed-widget/index.tsx
+++ src/features/pins-feed-widget/index.tsx
@@ -22,32 +22,34 @@ export const PinsFeedWidget = () => {
   return (
     <>
       <div className="w-full h-full overflow-y-auto">
-        <div className="py-2">
-          <NewPinWidget />
-        </div>
-
         {activeUser && (
-          <div className="px-2">
-            <div className="p-2 border rounded-sm shadow-md">
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline" size="sm">
-                    {view === 'Following' ? 'Following' : 'Global'}
-                  </Button>
-                </DropdownMenuTrigger>
-
-                <DropdownMenuContent align="start">
-                  <DropdownMenuRadioGroup
-                    value={view}
-                    onValueChange={(v) => setView(v as PinFeedView)}
-                  >
-                    <DropdownMenuRadioItem value="Global">Global</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="Following">Following</DropdownMenuRadioItem>
-                  </DropdownMenuRadioGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
+          <>
+            <div className="py-2">
+              <NewPinWidget />
             </div>
-          </div>
+
+            <div className="px-2">
+              <div className="p-2 border rounded-sm shadow-md">
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="sm">
+                      {view === 'Following' ? 'Following' : 'Global'}
+                    </Button>
+                  </DropdownMenuTrigger>
+
+                  <DropdownMenuContent align="start">
+                    <DropdownMenuRadioGroup
+                      value={view}
+                      onValueChange={(v) => setView(v as PinFeedView)}
+                    >
+                      <DropdownMenuRadioItem value="Global">Global</DropdownMenuRadioItem>
+                      <DropdownMenuRadioItem value="Following">Following</DropdownMenuRadioItem>
+                    </DropdownMenuRadioGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            </div>
+          </>
         )}
 
         {isLoading ? (
